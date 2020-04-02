d604c8013afb4ebd674e08d85bb2548502bb3efb
Your Name <you@example.com>
2020-04-02 17:01:52 +0500

    commit

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -49,6 +49,10 @@ let routes = [{
     {
         path: "/users",
         component: require("./components/Users.vue")
+    },
+    {
+        path: "/developer",
+        component: require("./components/Developer.vue")
     }
 ];
 
@@ -79,6 +83,21 @@ Vue.use(VueProgressBar, {
 
 let Fire = new Vue();
 window.Fire = Fire;
+
+Vue.component(
+    'passport-clients',
+    require('./components/passport/Clients.vue')
+);
+
+Vue.component(
+    'passport-authorized-clients',
+    require('./components/passport/AuthorizedClients.vue')
+);
+
+Vue.component(
+    'passport-personal-access-tokens',
+    require('./components/passport/PersonalAccessTokens.vue')
+);
 
 Vue.component(
     "example-component",
